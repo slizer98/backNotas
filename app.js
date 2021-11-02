5af2e0ec115040cb6f3eaa5e8eff5fe608e567ef
slizer98 <erick.m.villa.r@gmail.com>
2021-11-02 12:03:08 -0600

    Git de Arranque

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import mongoose from 'mongoose';
+
+
+const app = express();
+const uri = 'mongodb://localhost:27017/emvr-proyecto-express';
+
+const option = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+// conexion a la base de datos
+mongoose.connect(uri, option).then( 
+    () => {
+    console.log('Conexion a la base de datos establecida');
+    },
+    err => {
+    console.log(err);
+    }
+)
+
+// Middlewere
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Rutas
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+
+// Middleware para vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Puerto
+app.set('puerto', process.env.PORT || 3000)
+
+app.listen(app.get('puerto'), () =>{
+    console.log(`Escucnado en el puerto: ${app.get('puerto')}`);
+});
